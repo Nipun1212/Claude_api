5d50c23a59588784fecc23325f87084d4c3dd784
Nipun Bhatia <80609104+Nipun1212@users.noreply.github.com>
2023-07-20 18:49:56 +0800

    Uploaded to PyPI

---
+++ README.md
@@ -0,0 +1,3 @@
+# Claude AI-API ( Unofficial )
+
+This project provides an unofficial API for Claude AI from Anthropic, allowing users to access and interact with Claude AI and trying out experiments with the same.
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+base_path = Path(__file__).parent
+long_description = (base_path / "README.md").read_text()
+
+setup(
+    name='claude_api', 
+    version='0.1.0',  
+    author='Nipun',
+    license="MIT",
+    author_email='nipunbhatia06@gmail.com',
+    description='A Python package provides an unofficial api for interacting with the Claude from Anthropic',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Nipun1212/Claude_api',  # Replace with your GitHub repository URL
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords=['claude', 'ai', 'claude-ai', 'API', 'requests', 'chatbot'],
+    install_requires=[
+        'requests',  # List any dependencies your package needs
+    ],
+    python_requires=">=3.7",
+)
